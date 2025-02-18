50ea79355d8a9f5929f2ae7f68a62f7540572fb6
Khel Tan <kheltan.engineering@gmail.com>
2025-02-18 21:09:02 +0100

    feat: add volar support for Vue/TS projects

---
+++ nvim/.config/nvim/lua/config/lspconfig.lua
@@ -0,0 +1,21 @@
+local lspconfig = require("lspconfig")
+local mason = require("mason")
+
+mason.setup()
+
+lspconfig.volar.setup({
+  filetypes = { "vue" },
+  init_options = {
+    vue = {
+      hybridMode = false,
+    },
+    -- typescript = {
+    --   tsdk = "/usr/local/lib/node_modules/" .. "typescript/lib",
+    -- },
+  },
+
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = false -- disable formatting
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
+})
