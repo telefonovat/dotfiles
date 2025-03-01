e482df03535df070a09eff70d02aa37e94229762
Khel Tan <kheltan.engineering@gmail.com>
2025-03-01 16:47:26 +0100

    feat: add conform.nvim config so that prettierd formatting runs on code

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      vue = { "prettierd" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      css = { "prettierd" },
+      json = { "prettierd" },
+      yaml = { "prettierd" },
+      html = { "prettierd" },
+      nginx = {},
+    },
+  },
+}
