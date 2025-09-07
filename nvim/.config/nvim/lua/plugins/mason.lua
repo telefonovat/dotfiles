7733b48e31ef10e173f6d19f236d82c27a452003
telefon <kheltan.engineering@gmail.com>
2025-09-07 12:37:08 +0200

    fix: vtsls fix to work with Vue and TS projects

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,4 +1,17 @@
 return {
   { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    version = "^1.0.0",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {},
+        automatic_installation = false,
+        automatic_setup = false,
+        automatic_enable = false,
+        handlers = nil,
+      })
+    end,
+  },
 }
